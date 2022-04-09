1b6451a11dd4b577136abcdf7e030d484b547de9
Fede <fedelva8@gmail.com>
2022-04-09 00:48:20 -0300

    a

--- Delgado/Ejercicios del dia 8-4-2022/Program.cs
+++ Delgado/Ejercicios del dia 8-4-2022/Program.cs
@@ -10,21 +10,25 @@ namespace Ejercicios_del_dia_8_4_2022
     {
         public string nombre;
         public string apellido;
-        public int edad;
+        public DateTime fechaNacimiento;
 
 
-        public Persona(string nom, string ap, int ed)
+        public Persona(string nom, string ap, DateTime ed)
         {
             nombre = nom;
             apellido = ap;
-            edad = ed;
+            fechaNacimiento = ed;
         }
         public Persona()
         {
         }
         static void Main(string[] args)
         {
-            Persona per1 = new Persona("Juan", "");
+            Persona per1 = new Persona("Juan", "Domingo", new DateTime(1995, 4, 24));
+            Persona per2 = new Persona("Franco", "Jaurez", new DateTime(2000, 8, 15));
+            Persona per3 = new Persona("Bautista", "Jaime", new DateTime(2000, 8, 15));
+            Persona per4 = new Persona("Fernando", "De La Fuente", new DateTime(2000, 8, 15));
+            Persona per5 = new Persona("", "Jaurez", new DateTime(2000, 8, 15));
         }
     }
 }
