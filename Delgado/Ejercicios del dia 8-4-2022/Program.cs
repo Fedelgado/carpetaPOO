527b645c754f2f0b479028e7ce257bd86be2eeaa
Fede <fedelva8@gmail.com>
2022-04-08 23:54:06 -0300

    asd

---
+++ Delgado/Ejercicios del dia 8-4-2022/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios_del_dia_8_4_2022
+{
+    internal class Persona
+    {
+        public string nombre;
+        public string apellido;
+        public int edad;
+
+
+        public Persona(string nom, string ap, int ed)
+        {
+            nombre = nom;
+            apellido = ap;
+            edad = ed;
+        }
+        public Persona()
+        {
+        }
+        static void Main(string[] args)
+        {
+            Persona per1 = new Persona("Juan", "");
+        }
+    }
+}
