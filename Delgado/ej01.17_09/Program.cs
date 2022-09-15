54faea8783aa28f0961ab1b9cbd74a516c02ebaf
Fede <fedelva8@gmail.com>
2022-09-15 17:49:26 -0300

    asd

---
+++ Delgado/ej01.17_09/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej01._17_09
+{
+    public class Cuenta
+    {
+        string titular;          
+        double cantidad = 0;
+
+        public string Nombre
+        {
+            get { return titular; }
+            set { titular = value; }
+        }
+
+        public double Cantidad
+        {
+            get { return cantidad; }
+            set { cantidad = value; }
+        }
+
+        public Cuenta(string nom, double cant)
+        {
+            titular = nom;
+            cantidad = cant;
+        }
+
+        public void ingresar(double cantIng)
+        {
+            if (cantIng >= 0)
+            {
+                cantidad = cantidad + cantIng;
+            }
+            else
+            {
+            }
+        }
+
+        public void retirar(double cantRet)
+        {
+            if (cantidad - cantRet < 0)
+            {
+                cantidad = 0;   
+            }
+            else
+            {
+                cantidad = cantidad - cantRet;
+            }
+        }
+
+        public Cuenta()
+        {
+        }
+
+        internal class Program
+        {
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Delgado/ej02._17_09/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej02._17_09
+{
+    public class Persona
+    {
+        string nombre = "";
+        int edad = 0;
+        int DNI = 0;
+        int peso = 0;
+        char sexo = (char) Sexo.H;
+        float altura = 0;
+       public enum Sexo
+        {
+            H = 'H', 
+            M = 'M'
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+
+        public int Dni
+        {
+            get { return DNI; }
+            set { DNI = value; }
+        }
+
+        public float Altura
+        {
+            get { return altura; }
+            set { altura = value; }
+        }
+
+        public int Peso
+        {
+            get { return peso; }
+            set { peso = value; }
+        }
+
+        
+
+        public Persona(string nom, int edad, char sexo)
+        {
+            this.sexo = sexo;
+            Nombre = nom;
+            Edad = edad;
+            DNI = 0;
+            peso = 0;
+            altura = 0;
+        }
+
+        public Persona(string nom, int edad, char sex)
+        {
+            sexo = sex;
+            Nombre = nom;
+            Edad = edad;
+            Dni = dni;
+            Peso = peso;
+            Altura = altura;
+        }
+
+        public Persona()
+        { 
+        }
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // Persona pene = new Persona("Inutil", 15, (char) Sexo.H);
+        }
+    }
+}
