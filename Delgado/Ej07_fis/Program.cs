2a9889efab2560fe01a88628407d6a2e3986daec
Fede <fedelva8@gmail.com>
2022-10-13 17:49:20 -0300

    asd

---
+++ Delgado/Ej07_fis/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej07_fis
+{
+    public class Raices
+    {
+        double a = 0;
+        double b = 0;
+        double c = 0;
+
+        public double A { get { return a; } set { a = value; } }
+        public double B { get { return b; } set { b = value; } }
+        public double C { get { return c; } set { c = value; } }
+
+        public Raices(int a, int b, int c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+        public Raices()
+        {
+        }
+
+        public string obtenerRaices()
+        {
+            double raizNeg = (-b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
+            double raizPos = (-b - Math.Sqrt(Math.Pow(b, 2)- 4 * a * c)) / (2 * a);
+            return $"Las raíces son {raizNeg} y {raizPos}";
+        }
+
+        public string obtenerRaiz()
+        {
+            double raizNeg = (-b + Math.Sqrt(Math.Pow(b, 2)-4*a*c)) / (2 * a);
+            if(Math.Pow(b, 2) - 4 * a * c == 0 ) 
+            {
+                return $"La raíz es {raizNeg}";
+            }
+            else
+            {
+                return "Tiene mas de una raíz";
+            }
+            
+        }
+
+        public double getDiscriminante()
+        {
+            return Math.Pow(b, 2) - 4 * a * c;
+        }
+
+        public bool tieneRaices()
+        {
+            if (Math.Pow(b, 2) - 4 * a * c > 0)
+            {
+                return true;
+            }
+            else 
+                return false;
+        }
+
+        public bool tieneRaiz()
+        {
+            if (Math.Pow(b, 2) - 4 * a * c == 0)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public string calcular()
+        {
+            if (tieneRaices())
+            {
+                obtenerRaices();
+            }
+            else if (tieneRaiz())
+            {
+                obtenerRaiz();
+            }
+            else {
+                Console.WriteLine("No tiene soluciones");
+            }
+
+            return $"El valor de la Ordenada al Origen es {c}";
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Delgado/Ej08_fis/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej08_fis
+{
+    public class Aula
+    {
+        string nombre;
+        int edad;
+        char sexo;
+        string materia;
+        public string Nombre { get { return nombre; } }
+        public int Edad { get { return edad; } }
+        public char Sexo { get { return sexo; } }
+        private char comprobarSexo(char sexo)
+        {
+            if (sexo == 'H' || sexo == 'M')
+            {
+                return sexo;
+            }
+            else
+            {
+                return 'H';
+            }
+        }
+
+        public virtual void falta()
+        {
+
+        }
+        
+        public Aula()
+        {
+
+        }
+
+        public Aula(string nom, int edad, char sexo)
+        {
+            nombre = nom;
+            this.edad = edad;
+            this.sexo = comprobarSexo(sexo);
+        }
+
+        public Aula(string nom, int edad, char sexo, string materia)
+        {
+            nombre = nom;
+            this.edad = edad;
+            this.sexo = comprobarSexo(sexo);   
+            this.materia = materia;
+        }
+    }
+
+    public class Profesor : Aula
+    {
+        
+        static Random rdm = new Random();
+        int inasist = 0;
+        public Profesor()
+        {
+
+        }
+
+        public Profesor(string nom, int edad, char sexo, string materia) : base(nom, edad, sexo, materia)
+        {
+            
+        }
+
+        public override void falta()
+        {
+            int falta = rdm.Next(1, 100);
+              if (falta <= 20)
+            {
+                inasist++;
+            }
+            else
+            {
+
+            }
+        }
+
+    }
+
+    public class Alumno : Aula
+    {
+        int calificacion;
+        public int Calificacion { get { return calificacion; } }
+        public Alumno()
+        {
+        }
+
+        public Alumno(string nom, int edad, char sexo, int calif) : base(nom, edad, sexo)
+        { 
+            this.calificacion= calif;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
