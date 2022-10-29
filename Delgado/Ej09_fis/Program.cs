28e6c5af985d44ddaa39c15cbde9c2acc2711760
Fede <fedelva8@gmail.com>
2022-10-29 01:00:24 -0300

    asd

---
+++ Delgado/Ej09_fis/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej09_fis
+{
+    public class Cine
+    {
+        string pelicula;
+        int precioDeEnt = 500;
+
+        public Cine()
+        {
+
+        }
+
+    }
+
+    public class Asientos
+    {
+        int fila;
+        char col;
+        bool estaOcup;
+        
+
+    }
+
+    public class Pelicula
+    {
+        string titulo;
+        int duracion;
+        int edadMin;
+        string director;
+
+        public Pelicula()
+        {
+
+        }
+    }
+
+    public class Espectador 
+    {
+        string nombre;
+        int edad;
+        int dinDisp;
+        public Espectador()
+        {
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Asientos asiento = new Asientos();
+            Console.WriteLine(asiento.asientoSelec);
+            Console.ReadKey();
+        }
+    }
+}
