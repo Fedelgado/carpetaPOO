fb9eeecef7728862b7bc8cebcb2812321efccf9a
Fede <fedelva8@gmail.com>
2022-11-01 20:50:48 -0300

    asd

--- Delgado/Ej09_fis/Program.cs
+++ Delgado/Ej09_fis/Program.cs
@@ -1,10 +1,11 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+namespace Ej09_fis
+{
     public class Cine
     {
         string pelicula;
@@ -19,6 +20,7 @@ using System.Threading.Tasks;
 
     public class Asientos
     {
+        Random rd = new Random();
         int[] fila = {8,7,6,5,4,3,2,1};
         char[] letra = {'A','B','C','D','E','F','G','H','I'};
         
@@ -27,7 +29,11 @@ using System.Threading.Tasks;
             
         }
         
-        
+       public string asientoOcup()
+       {
+           string asiento = ""+fila[rd.Next(1,8)]+""+letra[rd.Next(1,9)]+"";
+		   return asiento;
+       }
 
     }
 
@@ -62,7 +68,7 @@ using System.Threading.Tasks;
         static void Main(string[] args)
         {
             Asientos asiento = new Asientos();
-            Console.WriteLine(asiento.asientoSelec);
+           // Console.WriteLine(asiento.asientoSelec);
             Console.ReadKey();
         }
     }
