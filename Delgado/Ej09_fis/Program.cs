b4ea121c283b04e54dc04bbb931d9819053160d9
Fede <fedelva8@gmail.com>
2022-11-02 18:55:57 -0300

    Ejercicio 9 terminado

--- Delgado/Ej09_fis/Program.cs
+++ Delgado/Ej09_fis/Program.cs
@@ -8,8 +8,8 @@ namespace Ej09_fis
 {
     public class Cine
     {
-        string pelicula;
-        int precioDeEnt = 500;
+        public string pelicula = "Jurassic Park";
+        public int precioDeEnt = 300;
 
         public Cine()
         {
@@ -29,7 +29,7 @@ namespace Ej09_fis
             
         }
         
-       public string asientoOcup()
+       public string asientoRan()
        {
            string asiento = ""+fila[rd.Next(1,8)]+""+letra[rd.Next(1,9)]+"";
 		   return asiento;
@@ -39,10 +39,10 @@ namespace Ej09_fis
 
     public class Pelicula
     {
-        string titulo;
-        int duracion;
-        int edadMin;
-        string director;
+        string titulo = "Jurassic Park";
+        int duracionMins = 90;
+        int edadMin = 15;
+        string director = "Joe Johnston";
 
         public Pelicula()
         {
@@ -53,22 +53,72 @@ namespace Ej09_fis
     public class Espectador 
     {
         string nombre;
+		public string Nombre { get {return nombre;}}
         int edad;
+		public int Edad { get {return edad;}}
         int dinDisp;
+		public int DinDisp { get {return dinDisp;}}
         string asiento;
+		public string Asiento { get {return asiento;} set {asiento = value;	}}
         bool puedeSent = false;
-        
+        public bool PuedeSent { get {return puedeSent;}}
+		
         public Espectador()
         {
 
         }
+		
+		public Espectador(string nom, int edad, int din) 
+		{
+			nombre = nom;
+			this.edad = edad;
+			dinDisp = din; 
+		}
+		
+		public void Sentarse()
+		{
+			if(DinDisp > 300 && Edad > 15)
+			{
+				puedeSent = true;
+			}
+		}
+		
     }
     internal class Program
     {
         static void Main(string[] args)
         {
             Asientos asiento = new Asientos();
-           // Console.WriteLine(asiento.asientoSelec);
+			List<string> AsientoOcup = new List<string>();
+			List<Espectador> espectadores = new List <Espectador>();
+			Espectador per1 = new Espectador("Juan", 18, 600);
+			Espectador per2 = new Espectador("Juana", 13, 200);
+			Espectador per3 = new Espectador("Pedro", 45, 350);
+			Espectador per4 = new Espectador("Fede", 19, 800);
+			Espectador per5 = new Espectador("Silvia", 5, 50);
+			Espectador per6 = new Espectador("Franchesca", 16, 200);
+			Espectador per7 = new Espectador("Pablo", 51, 400);
+			
+			espectadores.Add(per1);
+			espectadores.Add(per2);
+			espectadores.Add(per3);
+			espectadores.Add(per4);
+			espectadores.Add(per5);
+			espectadores.Add(per6);
+			espectadores.Add(per7);
+			
+			foreach (Espectador espectador in espectadores)
+			{
+				string AsientoRan = asiento.asientoRan();
+				while(AsientoOcup.Contains(AsientoRan))
+				{
+					AsientoRan = asiento.asientoRan();
+				}
+				
+				AsientoOcup.Add(AsientoRan);
+				espectador.Asiento = AsientoRan;
+				espectador.Sentarse();
+			}
             Console.ReadKey();
         }
     }
